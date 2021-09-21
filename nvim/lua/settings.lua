dc7d55bb80d4bad19fa091da156f36c43acbd096
home <home@example.com>
2021-09-21 13:47:59 -0500

    Added some initial settings

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,37 @@
+local fn = vim.fn
+local opt = vim.opt
+
+opt.mouse = "a"
+
+-- NVim UI
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+opt.ruler = true
+opt.number = true
+opt.cursorline = true
+opt.showtabline = 2
+opt.cmdheight = 2
+opt.pumheight = 10
+opt.showmatch = true
+opt.ignorecase = true -- ignore case letters in search
+opt.smartcase = true  -- ignore lowercase for whole pattern
+opt.splitbelow = true
+opt.splitright = true
+opt.signcolumn = "yes"
+
+-- Settings
+opt.hidden = true
+opt.history = 100
+
+-- Tabs, indents
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+opt.autoindent = true
+
+-- Other
+opt.updatetime = 300
+opt.timeoutlen = 500
+opt.undodir = fn.expand("$HOME") .. "/.vimdid"
+opt.undofile = true
